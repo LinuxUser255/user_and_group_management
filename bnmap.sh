ca7327f6b8274f1aff4e881096b6f890de5584f8
Chris <atnl@protonmail.com>
2019-02-26 08:46:32 -0500

    Add files via upload

---
+++ bnmap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+#This scan initiates service enumeration, syn scan, timing, and OS detection.  
+echo "Enter the target IP : " 
+
+read TargetIP
+
+nmap -p1-65535 -sV -T4 -A $TargetIP -oG BestnmapScan
+
+cat BestnmapScan
+
+./bnmap.sh
+
+
